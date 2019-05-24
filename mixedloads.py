62665b0f44afd0ee3accd63667d72e9914227ee6
davidspencer6174 <davidspencer6174@gmail.com>
2019-05-24 14:30:02 -0700

    Convert mixed loads to current route format
    Rewrite mixed-loads code to work with current route format. Used by the post-improvement function in utils.

--- mixedloads.py
+++ mixedloads.py
@@ -7,7 +7,6 @@ from locations import Student, School
 def mixed_loads(route_list):
     #Iterate over all routes and check whether they
     #can be removed.
-    num_routes_saved = 0
     print("Old number of routes: " + str(len(route_list)))
     i = 0
     while i < len(route_list):
@@ -15,56 +14,33 @@ def mixed_loads(route_list):
             if constants.VERBOSE:
                 print((len(route_list) - i))
         route_to_delete = route_list[i]
-        #Make backups to revert if the deletion fails
         for route in route_list:
             route.backup()
-        locations = route_to_delete.locations
-        #succeeded will be set to false if we find
-        #a student who cannot be moved.
+        stops = route_to_delete.stops
         succeeded = True
-        for location in locations:
-            #If this is a student, find a route to add to
-            if isinstance(location, Student):
-                best_cost = 100000
-                best_route = None
-                for route_to_add_to in route_list:
-                    #close_enough_school = False
-                    #for loc in route_to_add_to.locations:
-                    #    if isinstance(loc, School):
-                    #        if (constants.TRAVEL_TIMES[location.school.tt_ind,
-                    #                                  loc.tt_ind] < 1000 or
-                    #            constants.TRAVEL_TIMES[loc.tt_ind,
-                    #                                   location.school.tt_ind] < 1000):
-                    #            close_enough_school = True
-                    #            break
-                    #if not close_enough_school:
-                    #    continue
-                    route_to_add_to.temp_backup()
-                    prev_length = route_to_add_to.length
-                    if (route_to_add_to != route_to_delete and
-                        route_to_add_to.add_student(location)):
-                        cur_cost = route_to_add_to.length - prev_length
-                        if cur_cost < best_cost:
-                            best_cost = cur_cost
-                            best_route = route_to_add_to
-                    route_to_add_to.temp_restore()
-                if best_route == None:
-                    succeeded = False  #We were unable to move this student
+        for stop in stops:
+            added = False
+            for route_to_add_to in route_list:
+                if route_to_add_to == route_to_delete:
+                    continue
+                valid_type = True
+                for s2 in route_to_add_to.stops:
+                    if set([s2.type, stop.type]) == set(['H', 'E']):
+                        valid_type = False
+                        break
+                if valid_type and route_to_add_to.insert_mincost(stop):
+                    added = True
                     break
-                if not best_route.add_student(location):
-                    print("Something went wrong")
-        if not succeeded:  #Couldn't move all students; revert changes
+                #If the insertion failed, delete it
+                elif stop in route_to_add_to.stops:
+                    route_to_add_to.remove_stop(stop)
+            if not added:
+                succeeded = False
+                break
+        if succeeded:
+            del route_list[i]
+            print("Successfully deleted a route")
+        else:
             for route in route_list:
                 route.restore()
-        else:
-            if constants.VERBOSE:
-                print("Successfully deleted a route")
-            #Track number of saved buses
-            num_routes_saved += 1
-            del route_list[i]
-            i -= 1
-        i += 1
-        #if i > 200:  #for the purposes of obtaining quicker profiling results
-        #    break
-    print("New number of routes: " + str(len(route_list)))
-    return num_routes_saved
+            i += 1
