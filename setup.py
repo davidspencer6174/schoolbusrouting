87f89e29e38dc6b935ad318621be8cf733679725
davidspencer6174 <davidspencer6174@gmail.com>
2019-06-11 17:29:03 -0700

    Change setup to accept custom format
    I wrote a custom input format for the special ed students, since trying to parse the existing one seemed unwise. This change reads this new format.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import constants
 from locations import School, Stop, Student
 from utils import californiafy, timesecs
 
-#phonebooks: list of filenames for phonebooks
-#phonebooks are assumed to be in the format that was provided to
-#me in early November 2018
+#students_file: a format I am using for special ed students
+#Columns are latitude, longitude, grade level, human-readable
+#description of special ed types (not used), text description.
 #all_geocodes: filename for list of all geocodes. gives map from geocode to ind
 #geocoded_stops: file name for map from stop to geocode
 #geocoded_schools: file name for map from school to geocode
@@ -12,7 +12,7 @@ from utils import californiafy, timesecs
 #students, and a dict from schools to indices in the travel time matrix.
 #bell_sched: file name for which column 3 is cost center and
 #column 4 is start time
-def setup_students(phonebooks, all_geocodes, geocoded_stops,
+def setup_students(students_filename, all_geocodes, geocoded_stops,
                    geocoded_schools, bell_sched):
     
     stops = open(geocoded_stops, 'r')
@@ -58,60 +58,55 @@ def setup_students(phonebooks, all_geocodes, geocoded_stops,
         schools_inds_map[school] = codes_inds_map[schools_codes_map[school]]
     
     students = []
-    bus_col = 12
     #Maintain a dictionary of school indices to schools so that
     #school objects can be tested for equality.
     ind_school_dict = dict()
     #Maintain a set of all School objects to return                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     all_schools = set()
-    for pb_part_filename in phonebooks:
-        pb_part = open(pb_part_filename, 'r')
-        pb_part.readline()  #header
-        for student_record in pb_part.readlines():
-            fields = student_record.split(";")
-            if len(fields) <= bus_col + 6:
-                continue
-            if fields[bus_col + 6].strip() == ", ,":  #some buggy rows
-                continue
-            if fields[bus_col - 1].strip() == "9500":  #walker
-                continue
-            if fields[bus_col + 2].strip() not in ["1", "01"]:  #not first trip
-                continue
-            if fields[1].strip() == "":  #no school given
-                continue
-            #For now, I won't consider special ed.
-            if fields[5].strip() not in ["M", "X", "P"]:
-                continue
-            stop = californiafy(fields[bus_col + 6])
-            school = fields[1].strip()  #Cost center id
-            stop_ind = codes_inds_map[stops_codes_map[stop]]
-            school_ind = codes_inds_map[schools_codes_map[school]]
-            grade = fields[3].strip()  #Grade level
-            age_type = 'Other'
-            try:
-                grade = int(grade)
-            except:
-                grade = -1
-            if int(grade) in constants.GRADES_TYPE_MAP:
-                age_type = constants.GRADES_TYPE_MAP[int(grade)]
-            if age_type == 'Other':
-                print(grade)
-            if school_ind not in ind_school_dict:
-                belltime = 8*60*60  #default to 8AM start
-                #None of the 19xxxxx schools have start times.
-                if school in centers_times_map:
-                    belltime = centers_times_map[school]
-                else:
-                    if constants.VERBOSE:
-                        print("No time given for " + school)                    
-                ind_school_dict[school_ind] = School(school_ind, belltime, fields[2])
-                all_schools.add(ind_school_dict[school_ind])
-            this_student = Student(stop_ind, ind_school_dict[school_ind],
-                                   age_type, fields)
-            students.append(this_student)
-            schools_students_map[school].add(this_student)
-        pb_part.close()
-        
+    student_records = open(students_filename, 'r')
+    student_records.readline()  #header
+    for student_record in student_records.readlines():
+        fields = student_record.split(",")
+        school = fields[6].strip()
+        stop_ind = codes_inds_map[fields[0].strip() + ";" + fields[1].strip()]
+        school_ind = codes_inds_map[schools_codes_map[school]]
+        grade = fields[2].strip()
+        age_type = 'Other'
+        try:
+            grade = int(grade)
+        except:
+            grade = -1
+        if int(grade) in constants.GRADES_TYPE_MAP:
+            age_type = constants.GRADES_TYPE_MAP[int(grade)]
+        if age_type == 'Other':
+            print(grade)
+        if school_ind not in ind_school_dict:
+            belltime = 8*60*60  #default to 8AM start
+            #None of the 19xxxxx schools have start times.
+            if school in centers_times_map:
+                belltime = centers_times_map[school]
+            else:
+                if constants.VERBOSE:
+                    print("No time given for " + school)                    
+            ind_school_dict[school_ind] = School(school_ind, belltime, fields[2])
+            all_schools.add(ind_school_dict[school_ind])
+        this_student = Student(stop_ind, ind_school_dict[school_ind],
+                               age_type, fields)
+        students.append(this_student)
+        schools_students_map[school].add(this_student)
+        needs = fields[4].split(";")
+        #Add special needs
+        for need in needs:
+            if len(need) == 1:
+                #Most types of needs do not require extra info
+                assert (need in ["M", "W", "L", "A", "I", "F"]), ("Unknown need type"+str(need))
+                this_student.add_need(need)
+            else:
+                #Custom max travel time does require extra info
+                #Translate from minutes to seconds
+                assert (need[0] == "T"), ("Unknown need type"+str(need))
+                this_student.add_need(need[0], value = int(need[1:])*60)
+    student_records.close()
     return students, schools_students_map, all_schools
 
 
