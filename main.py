5874a0865225e74e4bcfaf8403e03501e2cdb5b9
Cu Hauw Hung <cuhauwhung@wifi-131-179-9-137.host.ucla.edu>
2019-06-23 14:29:48 -0700

    ran simualtions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from routing import start_routing, all_pairs_start_combining 
 from setup import setup_data
-from ds_setup import setup_ds_students, setup_ds_buses
 from wh_routestospec import wh_routes_to_spec
 from ds_routestospec import spec_to_ds_routes
 import ds_constants
@@ -26,21 +25,20 @@ def main():
 combined_clustered_routes = main()
 banana = unpack_routes(combined_clustered_routes)
 
-
 # import pickle
 # with open('/users/cuhauwhung/Desktop/two_unpacked_routes.pickle', 'rb') as fp:
 #     banana = pickle.load(fp)
 
 spec_routes = wh_routes_to_spec(banana)
 ds_routes = spec_to_ds_routes(spec_routes)
-improved_ds = improvement_procedures(ds_routes)
+#improved_ds = improvement_procedures(ds_routes)
 
 from ds_setup import setup_ds_students, setup_ds_buses
 from ds_bus_assignment_brute_force import assign_buses
 
 prefix = '/Users/cuhauwhung/Google Drive (cuhauwhung@g.ucla.edu)/Masters/Research/school_bus_project/Willy_Data/'
 buses = setup_ds_buses(prefix+'dist_bus_capacities.csv')
-final_routes = assign_buses(improved_ds, buses)
+final_routes = assign_buses(ds_routes, buses)
 
 from ds_diagnostics import metrics 
 metrics(final_routes)
